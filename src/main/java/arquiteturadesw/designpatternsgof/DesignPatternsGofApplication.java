75ff3160246801d8d41bcd154e2dba4ef1925573
Leila Farias <leila.s.oliveira@hotmail.com>
2022-03-06 18:56:46 -0300

    [feat] Initial project

---
+++ src/main/java/arquiteturadesw/designpatternsgof/DesignPatternsGofApplication.java
@@ -0,0 +1,13 @@
+package arquiteturadesw.designpatternsgof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsGofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsGofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arquiteturadesw/designpatternsgof/DesignPatternsGofApplicationTests.java
@@ -0,0 +1,13 @@
+package arquiteturadesw.designpatternsgof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsGofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
